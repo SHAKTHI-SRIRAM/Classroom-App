40191955ebf0bb2d99536f3c635439e5957fbedd
Shakthidhar Vilvanathan <Shakthi91124@users.noreply.github.com>
2020-10-05 16:52:23 +0530

    Created test classroom database

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import Classroom, Test, Question, Choice
 
 # Register your models here.
+class ClassroomAdmin(admin.ModelAdmin):
+    list_display = ['classname', 'teacher1', 'teacher2', 'teacher3']
+    search_fields = ['classname']
+    class Meta:
+        model = Classroom
+
+admin.site.register(Classroom, ClassroomAdmin)
+
+
+class TestAdmin(admin.ModelAdmin):
+    list_display = ['classroom', 'title']
+    search_fields = ['title']
+    class Meta:
+        model = Test
+
+admin.site.register(Test, TestAdmin)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['test', 'question']
+    search_fields = ['question']
+    class Meta:
+        model = Question
+
+admin.site.register(Question, QuestionAdmin)
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['choice', 'is_correct']
+    search_fields = ['choice']
+    class Meta:
+        model = Choice
+
+admin.site.register(Choice, ChoiceAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Classroom(models.Model):
+    classname = models.CharField(max_length=80)
+    teacher1 = models.CharField(max_length=30)
+    teacher2 = models.CharField(max_length=30, null=True, blank=True)
+    teacher3 = models.CharField(max_length=30, null=True, blank=True)
+
+    def __str__(self):
+        return self.classname
+
+
+class Test(models.Model):
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    deadline = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+class Question(models.Model):
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    question = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.question
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.CharField(max_length=100)
+    is_correct = models.BooleanField()
+
+    def __str__(self):
+        return self.choice 
+
+
